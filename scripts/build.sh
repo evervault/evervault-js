580d0ff59fecfd94ea8e207f653b14b9c414d20a
Jake Grogan <jake.kgrogan@gmail.com>
2022-07-26 16:30:06 +0100

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,3 @@
 find ./lib -maxdepth 1 -type d -exec ./node_modules/.bin/webpack --mode=production --config lib/v1/webpack.config.js   \;
+
+find ./lib -maxdepth 1 -type d -exec ./node_modules/.bin/webpack --mode=production --config lib/v2/webpack.config.js   \;
