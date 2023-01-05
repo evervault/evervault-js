b407aaa24e275e8f260cf5f5cf6b421cb4050e88
Eoin Power-Moran <eoinpm@evervault.com>
2023-01-05 17:46:37 +0000

    chore: use env vars

--- test/encryption.js
+++ test/encryption.js
@@ -4,8 +4,8 @@ const Evervault = require('../lib/v2/index.js');
 require('./utils').runBrowserJsPolyfills();
 
 const encryptedStringRegex = /((ev(:|%3A))(debug(:|%3A))?(([A-z0-9+/=%]+)(:|%3A))?((number|boolean|string)(:|%3A))?(([A-z0-9+/=%]+)(:|%3A)){3}(\$|%24))|(((eyJ[A-z0-9+=.]+){2})([\w]{8}(-[\w]{4}){3}-[\w]{12}))/
-const ev = new Evervault('team_fc26733f1539', 'app_275032e00186');
-const evDebug = new Evervault('team_fc26733f1539', 'app_275032e00186', { isDebugMode: true });
+const ev = new Evervault(process.env.EV_TEAM_UUID, process.env.EV_APP_UUID);
+const evDebug = new Evervault(process.env.EV_TEAM_UUID, process.env.EV_APP_UUID, { isDebugMode: true });
 
 describe('Encryption', () => {
   beforeEach(() => ev.loadKeys());
