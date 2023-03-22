62930bb64e8adcafef6d4955264651c99f36e947
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2023-03-22 16:38:51 +0000

    chore: more lint

--- e2e-app/scaffold-test-object.js
+++ e2e-app/scaffold-test-object.js
@@ -4,26 +4,26 @@ const EV_TEAM_UUID = searchParams.get("team");
 const EV_APP_UUID = searchParams.get("app");
 
 const ev = new window.Evervault(
-    EV_TEAM_UUID,
-    EV_APP_UUID,
+  EV_TEAM_UUID,
+  EV_APP_UUID,
 )
 
 const encryptForm = document.getElementById("ev-encrypt-form");
 encryptForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(encryptForm);
+  const formData = new FormData(encryptForm);
 
-    const objectToEncrypt = {
-        name: formData.get("ev-name-input"),
-        employer: {
-            name: formData.get("ev-employer-name-input"),
-            location: formData.get("ev-employer-location-input"),
-            current: formData.get("ev-employer-current-input"),
-        },
-        yearOfBirth: formData.get("ev-year-of-birth-input"),
-    }
+  const objectToEncrypt = {
+    name: formData.get("ev-name-input"),
+    employer: {
+      name: formData.get("ev-employer-name-input"),
+      location: formData.get("ev-employer-location-input"),
+      current: formData.get("ev-employer-current-input"),
+    },
+    yearOfBirth: formData.get("ev-year-of-birth-input"),
+  }
 
-    const encryptedValue = await ev.encrypt(objectToEncrypt);
-    document.getElementById("ev-encrypt-output").innerHTML = JSON.stringify(encryptedValue, null, 2);
+  const encryptedValue = await ev.encrypt(objectToEncrypt);
+  document.getElementById("ev-encrypt-output").innerHTML = JSON.stringify(encryptedValue, null, 2);
 });
--- e2e-app/scaffold-test-single.js
+++ e2e-app/scaffold-test-single.js
@@ -4,18 +4,18 @@ const EV_TEAM_UUID = searchParams.get("team");
 const EV_APP_UUID = searchParams.get("app");
 
 const ev = new window.Evervault(
-    EV_TEAM_UUID,
-    EV_APP_UUID,
+  EV_TEAM_UUID,
+  EV_APP_UUID,
 )
 
 const encryptForm = document.getElementById("ev-encrypt-form");
 encryptForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    console.log('test')
+  e.preventDefault();
+  console.log('test')
 
-    const formData = new FormData(encryptForm);
+  const formData = new FormData(encryptForm);
 
-    const value = formData.get("ev-encrypt-input");
-    const encryptedValue = await ev.encrypt(value);
-    document.getElementById("ev-encrypt-output").innerHTML = encryptedValue;
+  const value = formData.get("ev-encrypt-input");
+  const encryptedValue = await ev.encrypt(value);
+  document.getElementById("ev-encrypt-output").innerHTML = encryptedValue;
 });
