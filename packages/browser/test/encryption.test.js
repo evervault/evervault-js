b0c3060c40c27ff7eefec7b78653e7ac2f1b33d0
Eoin Power-Moran <eoinpm@evervault.com>
2023-05-09 16:09:39 +0100

    test: fix tests

--- packages/browser/test/encryption.test.js
+++ packages/browser/test/encryption.test.js
@@ -3,8 +3,8 @@ import { Buffer } from "node:buffer";
 import { describe, assert, it, beforeEach, expect } from "vitest";
 
 import Evervault from "../lib/main";
-import { crc32 } from "../lib/utils/crc32";
 import { setupCrypto } from "./setup";
+import crc32 from "crc-32";
 
 const encryptedStringRegex =
   /((ev(:|%3A))(debug(:|%3A))?(([A-z0-9+/=%]+)(:|%3A))?((number|boolean|string)(:|%3A))?(([A-z0-9+/=%]+)(:|%3A)){3}(\$|%24))|(((eyJ[A-z0-9+=.]+){2})([\w]{8}(-[\w]{4}){3}-[\w]{12}))/;
