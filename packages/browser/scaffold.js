7504c35a81690b818b115c4f03ba6f3233874f6b
Ciarán Curley <ciaran@evervault.com>
2023-05-24 16:10:11 +0100

    fix: remove ids in wrong place

--- packages/browser/scaffold.js
+++ packages/browser/scaffold.js
@@ -7,7 +7,7 @@ import Evervault from "./lib/main";
   );
   console.log(await evervault.encrypt("hello evervault"));
 
-  const inputs = evervault.inputs("ev-inputs", {teamId: "team_8eba95118afb", appId: "app_dac1f7c3c73d"});
+  const inputs = evervault.inputs("ev-inputs");
 
   const hook = inputs.on("change", async (encryptedCardData) => {
     console.log(encryptedCardData);
