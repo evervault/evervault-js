f9dcaad5307d911913d23d3991776c26742abcdc
Ciarán Curley <ciaran@evervault.com>
2023-06-08 11:13:37 +0100

    fix: format

--- packages/browser/env.d.ts
+++ packages/browser/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_EV_TEAM_UUID: string
-    readonly VITE_EV_APP_UUID: string
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
+  readonly VITE_EV_TEAM_UUID: string;
+  readonly VITE_EV_APP_UUID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
