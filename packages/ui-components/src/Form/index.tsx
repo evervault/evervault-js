636c55cfff031e6f8ed4ddf892cf62232554af52
Eoin Boylan <jb.eoin@gmail.com>
2024-05-20 22:53:59 +0100

    wip

--- packages/ui-components/src/Form/index.tsx
+++ packages/ui-components/src/Form/index.tsx
@@ -1,7 +1,26 @@
 import type { FormConfig } from "./types";
+import {useEffect, useState} from "react";
+import {useMessaging} from "../utilities/useMessaging";
 
 export function Form({ config }: { config: FormConfig }) {
-  console.log(config);
+  const [data, setData] = useState<JSON | null>(null);
+
+    useEffect(() => {
+        async function makeRequest() {
+            try {
+                const response = await fetch("https://api.evervault.com/forms", {
+                    headers: {
+                        'X-EVERVAULT-APP-ID': 'app_f4877d5d1d6a'
+                    }
+                });
+                console.log("response", response.status);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        void makeRequest()
+    }, ["http://localhost:3000/health"]);
   return (
       <div>
         <p>Rendering the Form</p>
