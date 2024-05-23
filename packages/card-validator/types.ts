e9830eb4da662109fd948e6188c97d874e01836d
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2024-05-23 14:27:13 +0100

    ts ignore example

--- packages/card-validator/types.ts
+++ packages/card-validator/types.ts
@@ -4,38 +4,38 @@ export type NumberValidationRules = {
   luhnCheck: boolean;
   ranges: number[] | [number[]];
   lengths: number[];
-}
+};
 
 export type SecurityCodeValidationRules = {
   length: number;
-}
+};
 
 export type CardBrand = {
   name: CardBrandName;
   isLocal: boolean;
   numberValidationRules: NumberValidationRules;
   securityCodeValidationRules: SecurityCodeValidationRules;
-}
+};
 
 export type CardValidationOptions = {
   acceptedBrands?: CardBrandName[];
-}
+};
 
 export type CardNumberValidationResult = {
   brand: CardBrandName | null;
   localBrands: CardBrandName[];
-  bin: string | null | undefined;
-  lastFour: string | null | undefined;
+  bin: string | null;
+  lastFour: string | null;
   isValid: boolean;
-}
+};
 
 export type CardExpiryValidationResult = {
-  month: string | null | undefined;
-  year: string | null | undefined;
+  month: string | null;
+  year: string | null;
   isValid: boolean;
-}
+};
 
 export type CardCVCValidationResult = {
-  cvc: string | null | undefined;
+  cvc: string | null;
   isValid: boolean;
-}
+};
