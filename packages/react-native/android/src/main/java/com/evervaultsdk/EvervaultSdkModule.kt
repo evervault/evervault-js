ab3fce5744208055dbda3419c948b7a4d589af95
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2024-05-24 15:52:43 +0100

    create the android bridge (#302)
    * create the android bridge
    
    * fix lockfile

--- packages/react-native/android/src/main/java/com/evervaultsdk/EvervaultSdkModule.kt
+++ packages/react-native/android/src/main/java/com/evervaultsdk/EvervaultSdkModule.kt
@@ -1,22 +1,51 @@
 package com.evervaultsdk
 
+import com.evervault.sdk.Evervault
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.WritableMap
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class EvervaultSdkModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
+  private val evervaultScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
   override fun getName(): String {
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun initialize(teamUuid: String, appUuid: String, promise: Promise) {
+    try {
+      Evervault.shared.configure(teamUuid, appUuid)
+      promise.resolve(null)
+    } catch (e: Exception) {
+      promise.reject("InitializationError", "Failed to initialize Evervault SDK", e)
+    }
+  }
+
+  @ReactMethod
+  fun encrypt(value: String, promise: Promise) {
+    evervaultScope.launch {
+      try {
+        val encrypted = withContext(context = Dispatchers.IO) {
+          Evervault.shared.encrypt(value)
+        }
+
+        promise.resolve(encrypted)
+      } catch (e: Exception) {
+        promise.reject("EncryptionError", "Failed to encrypt with the Evervault SDK $e")
+      }
+    }
   }
 
   companion object {
