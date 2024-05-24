ba24884261ccf46426f907a1bb2983a51ea59d6e
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2024-05-24 13:46:48 +0100

    create the android bridge

--- packages/react-native/android/src/main/java/com/evervaultsdk/EvervaultSdkPackage.kt
+++ packages/react-native/android/src/main/java/com/evervaultsdk/EvervaultSdkPackage.kt
@@ -1,10 +1,10 @@
 package com.evervaultsdk
 
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptModule
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-
 
 class EvervaultSdkPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
