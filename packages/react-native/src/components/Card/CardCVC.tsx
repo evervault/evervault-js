23e9c2ed1c761f037f565ee9cdb3d88fc910297f
Mark Doyle <mark@mdoyle.sh>
2024-08-12 17:37:25 +0100

    Clean up Set<CardField> state code (#373)

--- packages/react-native/src/components/Card/CardCVC.tsx
+++ packages/react-native/src/components/Card/CardCVC.tsx
@@ -2,10 +2,10 @@ import { validateNumber } from '@evervault/card-validator';
 import * as React from 'react';
 import { useEffect, useMemo } from 'react';
 import { TextInputMask } from 'react-native-masked-text';
-import { useCardContext } from './context';
+import { removeFieldFromSet, useCardContext } from './context';
 import { BaseProps } from './Card';
 
-export interface CVCProps extends BaseProps { }
+export interface CVCProps extends BaseProps {}
 
 export const CardCVC = ({
   style,
@@ -29,11 +29,8 @@ export const CardCVC = ({
 
   useEffect(() => {
     context.setRegisteredFields((prev) => new Set(prev).add('cvc'));
-    return () => context.setRegisteredFields((prev) => {
-      const next = new Set(prev);
-      next.delete('cvc');
-      return next;
-    });
+    return () =>
+      context.setRegisteredFields((prev) => removeFieldFromSet(prev, 'cvc'));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
--- packages/react-native/src/components/Card/CardNumber.tsx
+++ packages/react-native/src/components/Card/CardNumber.tsx
@@ -2,7 +2,7 @@ import { validateNumber } from '@evervault/card-validator';
 import * as React from 'react';
 import { useEffect, useMemo, useRef } from 'react';
 import { TextInputMask } from 'react-native-masked-text';
-import { useCardContext } from './context';
+import { removeFieldFromSet, useCardContext } from './context';
 import { BaseProps } from './Card';
 
 interface CardNumberProps extends BaseProps {
@@ -40,11 +40,9 @@ export function CardNumber({
 
   useEffect(() => {
     context.setRegisteredFields((prev) => new Set(prev).add('number'));
-    return () => context.setRegisteredFields((prev) => {
-      const next = new Set(prev);
-      next.delete('name');
-      return next;
-    });
+
+    return () =>
+      context.setRegisteredFields((prev) => removeFieldFromSet(prev, 'number'));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
