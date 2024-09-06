606ff1dfcf11781eeb9b40efc2f925305c1df2cf
Donal Tuohy <donaltuohy@gmail.com>
2024-09-06 09:43:10 +0100

    [ETR-2493] Add container style to webview

--- packages/react-native/src/components/3DS/ThreeDSecureFrame.tsx
+++ packages/react-native/src/components/3DS/ThreeDSecureFrame.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
 import { useEvervault } from "../EvervaultProvider";
 import WebView from "react-native-webview";
 import { CHALLENGE_DOMAIN_3DS } from "./config";
@@ -10,19 +10,16 @@ interface ThreeDSecureFrameProps {
   sessionId: string;
 }
 
-/*
-    Use the ThreeDSecureFrame component to display the 3DS challenge webview frame in your app.
-*/
 export function ThreeDSecureFrame({ sessionId }: ThreeDSecureFrameProps) {
   const { teamUuid, appUuid } = useEvervault();
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   if (!teamUuid || !appUuid) {
     throw new Error('Missing Evervault Team or App Uuid. Make sure the ThreeDSecureFrame is nested within the Evervault Provider');
   }
 
   return (
-    <View style={[defaultStyles.threeDSFrame]}>
+    <View style={defaultStyles.threeDSFrame}>
       {loading && (
         <View style={defaultStyles.spinnerOverlay}>
           <Spinner />
@@ -30,11 +27,12 @@ export function ThreeDSecureFrame({ sessionId }: ThreeDSecureFrameProps) {
       )}
       <WebView
         source={{
-          uri: `https://${CHALLENGE_DOMAIN_3DS}/?session=${sessionId}&app=${appUuid}&team=${teamUuid}`
+          uri: `https://${CHALLENGE_DOMAIN_3DS}/?session=${sessionId}&app=${appUuid}&team=${teamUuid}`,
         }}
         containerStyle={defaultStyles.webView}
-        onLoadStart={(_) => setLoading(true)}
-        onLoadEnd={(_) => setLoading(false)}
+        onLoadStart={() => setLoading(true)}
+        onLoadEnd={() => setLoading(false)}
+        overScrollMode="content"
       />
     </View>
   );
