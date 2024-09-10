ae7ec0bc4cd2be893d4d2de539af566d47c4410f
Donal Tuohy <donaltuohy@gmail.com>
2024-09-10 16:00:35 +0100

    Update tests and remove test build

--- packages/browser/test/encryption.test.ts
+++ packages/browser/test/encryption.test.ts
@@ -270,7 +270,14 @@ describe("Encryption with evervault async initailization", () => {
     context.evDebug = await Evervault.init(
       import.meta.env.VITE_EV_TEAM_UUID,
       import.meta.env.VITE_EV_APP_UUID,
-      { isDebugMode: true }
+      { 
+        isDebugMode: true,
+        urls: {
+          keysUrl: import.meta.env.VITE_KEYS_URL as string,
+          apiUrl: import.meta.env.VITE_API_URL as string,
+          componentsUrl: import.meta.env.VITE_UI_COMPONENTS_URL as string,
+        },
+      }
     );
     context.evCustomClient = await Evervault.init(
       import.meta.env.VITE_EV_TEAM_UUID,
