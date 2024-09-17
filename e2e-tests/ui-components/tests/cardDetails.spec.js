283daa3b9a4083ae00749a295ee0203ce346e615
Thomas Cullen <thomas@evervault.com>
2024-09-17 12:53:24 +0100

    Add tests

--- e2e-tests/ui-components/tests/cardDetails.spec.js
+++ e2e-tests/ui-components/tests/cardDetails.spec.js
@@ -533,4 +533,61 @@ test.describe("card component", () => {
       await expect(frame.getByLabel("Expiration")).toHaveValue(digit);
     });
   });
+
+  test("Revalidates CVC when card number changes", async ({ page }) => {
+    let values = {};
+
+    await page.exposeFunction("handleChange", (newValues) => {
+      values = newValues;
+    });
+
+    await page.evaluate(() => {
+      const card = window.evervault.ui.card();
+      card.on("change", window.handleChange);
+      card.mount("#form");
+    });
+
+    const frame = page.frameLocator("iframe[data-evervault]");
+    // anter amex card which requires 4 digit cvc
+    await frame.getByLabel("Number").fill("378282246310005");
+    await frame.getByLabel("CVC").fill("123");
+    await frame.getByLabel("CVC").blur();
+    await expect(frame.getByText("Your CVC is invalid")).toBeVisible();
+    await expect.poll(async () => values.errors?.cvc).not.toBeUndefined();
+    await frame.getByLabel("Number").clear();
+    await frame.getByLabel("Number").fill("4242424242424242");
+    await expect.poll(async () => values.errors?.cvc).toBeUndefined();
+    await expect(frame.getByText("Your CVC is invalid")).not.toBeVisible();
+  });
+
+  test("Updates the underlying CVC when switching from 4 digit CVC to 3", async ({
+    page,
+  }) => {
+    let lastChange = {};
+
+    await page.exposeFunction("handleChange", (newValues) => {
+      lastChange = newValues;
+    });
+
+    await page.evaluate(() => {
+      const card = window.evervault.ui.card();
+      card.on("change", window.handleChange);
+      card.mount("#form");
+    });
+
+    const frame = page.frameLocator("iframe[data-evervault]");
+    // anter amex card which requires 4 digit cvc
+    await frame.getByLabel("Number").fill("378282246310005");
+    await frame.getByLabel("Expiration").fill("1228");
+    await frame.getByLabel("CVC").fill("1234");
+    await expect.poll(async () => lastChange.isComplete).toBeTruthy();
+    // switch to visa card, should still be valid as CVC truncated to 123
+    await frame.getByLabel("Number").clear();
+    await frame.getByLabel("Number").fill("4242424242424242");
+    await expect.poll(async () => lastChange.isComplete).toBeTruthy();
+    // switch back to amex, should be invalid as CVC is now 3 digits
+    await frame.getByLabel("Number").clear();
+    await frame.getByLabel("Number").fill("378282246310005");
+    await expect.poll(async () => lastChange.isComplete).toBeFalsy();
+  });
 });
