0033f5bae65be1dac994dd3d5b115ad0b2011e9f
Thomas Cullen <thomas@evervault.com>
2024-09-18 09:12:48 +0100

    Remove only

--- e2e-tests/ui-components/tests/cardDetails.spec.js
+++ e2e-tests/ui-components/tests/cardDetails.spec.js
@@ -7,7 +7,7 @@ test.describe("card component", () => {
   });
 
   Object.values(VALID_CARDS).forEach((card) => {
-    test.only(`can capture valid card details for ${card.number} (${card.brand})`, async ({
+    test(`can capture valid card details for ${card.number} (${card.brand})`, async ({
       page,
     }) => {
       let values = {};
