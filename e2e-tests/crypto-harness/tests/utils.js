2a5ac58d759613591277440c51c613131f52b921
Liam <liam@evervault.com>
2025-05-28 11:21:55 +0100

    chore: fmt

--- e2e-tests/crypto-harness/tests/utils.js
+++ e2e-tests/crypto-harness/tests/utils.js
@@ -11,38 +11,38 @@ function generateEcdhKeyPair(curve) {
 }
 
 export function generateKeyPairs() {
-  const koblitzKeys = generateEcdhKeyPair('secp256k1');
-  const r1Keys = generateEcdhKeyPair('prime256v1');
+  const koblitzKeys = generateEcdhKeyPair("secp256k1");
+  const r1Keys = generateEcdhKeyPair("prime256v1");
 
   return {
     publicKeys: {
-      ecdhKey: koblitzKeys.publicKey.toString('base64'),
-      ecdhP256Key: r1Keys.publicKey.toString('base64'),
-      ecdhP256KeyUncompressed: r1Keys.uncompressedPublicKey.toString('base64'),
+      ecdhKey: koblitzKeys.publicKey.toString("base64"),
+      ecdhP256Key: r1Keys.publicKey.toString("base64"),
+      ecdhP256KeyUncompressed: r1Keys.uncompressedPublicKey.toString("base64"),
     },
     privateKeys: {
-      ecdhPrivateKey: koblitzKeys.privateKey.toString('base64'),
-      ecdhP256PrivateKey: r1Keys.privateKey.toString('base64'),
-    }
-  }
+      ecdhPrivateKey: koblitzKeys.privateKey.toString("base64"),
+      ecdhP256PrivateKey: r1Keys.privateKey.toString("base64"),
+    },
+  };
 }
 
 export function memoize(fn) {
   const invocationMap = {};
   return (...args) => {
-    console.log('memoized function entered');
+    console.log("memoized function entered");
     const serializedArgs = JSON.stringify(args);
-    const hasher = createHash('sha1');
+    const hasher = createHash("sha1");
     hasher.update(serializedArgs);
-    const hashedArgs = hasher.digest('hex');
-    console.log('hashed arguments', hashedArgs);
-    if(invocationMap[hashedArgs] != null) {
-      console.log('returning memoized invocation');
+    const hashedArgs = hasher.digest("hex");
+    console.log("hashed arguments", hashedArgs);
+    if (invocationMap[hashedArgs] != null) {
+      console.log("returning memoized invocation");
       return invocationMap[hashedArgs];
     }
     const invocation = fn(...args);
     invocationMap[hashedArgs] = invocation;
-    console.log('tracking invocation');
+    console.log("tracking invocation");
     return invocation;
-  }
+  };
 }
