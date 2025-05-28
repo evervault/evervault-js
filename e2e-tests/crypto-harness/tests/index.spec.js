712be87389b52f815f891c282c2d145815d41e1e
Liam <liam@evervault.com>
2025-05-28 11:21:11 +0100

    chore: flesh out harness so it can produce equivalent ciphertexts in both libraries

--- e2e-tests/crypto-harness/tests/index.spec.js
+++ e2e-tests/crypto-harness/tests/index.spec.js
@@ -1,6 +1,61 @@
+import { env } from "node:process";
 import { test, expect } from "@playwright/test";
+import { generateKeyPairs, memoize } from "./utils";
+
+let keyPairs;
+
+test.beforeAll(async () => {
+  keyPairs = generateKeyPairs();
+});
 
 test.beforeEach(async ({ page }) => {
+  page.addInitScript(() => {
+    let testTime = Date.now();
+    Date.now = () => testTime;
+
+    crypto.getRandomValues = (buffer) => {
+      console.log("intercepted random values called");
+      const arr = new Array(buffer.byteLength);
+      const zeroedBuf = new Uint8Array(arr);
+      buffer.buffer = zeroedBuf;
+    };
+
+    const originalGenerateKey = crypto.subtle.generateKey.bind(crypto.subtle);
+    function memoize(fn) {
+      const invocationMap = {};
+      return (...args) => {
+        console.log("memoized function entered");
+        const serializedArgs = JSON.stringify(args);
+        if (invocationMap[serializedArgs] != null) {
+          console.log("returning memoized invocation");
+          return invocationMap[serializedArgs];
+        }
+        const invocation = fn(...args);
+        invocationMap[serializedArgs] = invocation;
+        console.log("tracking invocation");
+        return invocation;
+      };
+    }
+
+    crypto.subtle.generateKey = memoize(originalGenerateKey);
+  });
+
+  await page.route(`${env.VITE_KEYS_URL}/*/apps/*?*`, (route) => {
+    const reqUrl = route.request().url();
+    const path = reqUrl.slice(env.VITE_KEYS_URL.length);
+    const [_leadingSlash, teamUuid, _apps, appUuid] = path.split("/");
+    return route.fulfill({
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        appUuid,
+        teamUuid,
+        ...keyPairs.publicKeys,
+      }),
+    });
+  });
   await page.goto("http://localhost:4009");
   await page.waitForFunction(() => window.evervault);
   await page.waitForFunction(() => window.encryption);
@@ -12,14 +67,16 @@ test("Always pass", async ({ page }) => {
     result = res;
   });
 
+  const plaintextValue = "plaintext";
   page.evaluate((payload) => {
     window.evervault.encrypt(payload).then((res) => {
       window.setResult(res);
     });
-  }, "evervault");
+  }, plaintextValue);
 
   await expect.poll(() => result).not.toBe(undefined);
 
+  const browserEncryptedValue = result;
   page.evaluate(() => {
     window.setResult(undefined);
   });
@@ -29,6 +86,7 @@ test("Always pass", async ({ page }) => {
     window.encryption.encrypt(payload).then((res) => {
       window.setResult(res);
     });
-  }, "evervault");
+  }, plaintextValue);
   await expect.poll(() => result).not.toBe(undefined);
+  await expect.poll(() => result).toEqual(browserEncryptedValue);
 });
---
+++ e2e-tests/crypto-harness/tests/utils.js
@@ -0,0 +1,48 @@
+import { createECDH, createHash } from "node:crypto";
+
+function generateEcdhKeyPair(curve) {
+  const ecdh = createECDH(curve);
+  ecdh.generateKeys();
+  return {
+    uncompressedPublicKey: ecdh.getPublicKey(null, "uncompressed"),
+    publicKey: ecdh.getPublicKey(null, "compressed"),
+    privateKey: ecdh.getPrivateKey(null, "compressed"),
+  };
+}
+
+export function generateKeyPairs() {
+  const koblitzKeys = generateEcdhKeyPair('secp256k1');
+  const r1Keys = generateEcdhKeyPair('prime256v1');
+
+  return {
+    publicKeys: {
+      ecdhKey: koblitzKeys.publicKey.toString('base64'),
+      ecdhP256Key: r1Keys.publicKey.toString('base64'),
+      ecdhP256KeyUncompressed: r1Keys.uncompressedPublicKey.toString('base64'),
+    },
+    privateKeys: {
+      ecdhPrivateKey: koblitzKeys.privateKey.toString('base64'),
+      ecdhP256PrivateKey: r1Keys.privateKey.toString('base64'),
+    }
+  }
+}
+
+export function memoize(fn) {
+  const invocationMap = {};
+  return (...args) => {
+    console.log('memoized function entered');
+    const serializedArgs = JSON.stringify(args);
+    const hasher = createHash('sha1');
+    hasher.update(serializedArgs);
+    const hashedArgs = hasher.digest('hex');
+    console.log('hashed arguments', hashedArgs);
+    if(invocationMap[hashedArgs] != null) {
+      console.log('returning memoized invocation');
+      return invocationMap[hashedArgs];
+    }
+    const invocation = fn(...args);
+    invocationMap[hashedArgs] = invocation;
+    console.log('tracking invocation');
+    return invocation;
+  }
+}
