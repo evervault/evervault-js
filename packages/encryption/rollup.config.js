b901d84dd9da02cd4ae02d2031fd4754e0d534cb
Thomas Cullen <thomas@evervault.com>
2025-05-28 15:59:12 +0100

    Create dedicated encryption package (#673)
    * Scaffold new encryption package
    
    * Add dedicated encryption package
    
    * Add @rollup/plugin-node-resolve to resolve dependencies for encryption package
    
    * chore: revert changes to ui-components to isolate changes
    
    ---------
    
    Co-authored-by: Liam <liam@evervault.com>
    Co-authored-by: Liam Farrelly <github@l3f7.dev>

---
+++ packages/encryption/rollup.config.js
@@ -0,0 +1,23 @@
+import typescript from "@rollup/plugin-typescript";
+import replace from "@rollup/plugin-replace";
+import dotenv from "dotenv";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+dotenv.config({ path: "../../.env" });
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "es",
+  },
+  plugins: [
+    nodeResolve(),
+    typescript(),
+    replace({
+      preventAssignment: true,
+      values: {
+        "process.env.VITE_KEYS_URL": JSON.stringify(process.env.VITE_KEYS_URL),
+      },
+    }),
+  ],
+};
---
+++ packages/encryption/src/utils/crc32.ts
@@ -0,0 +1,23 @@
+const crcTable = new Uint32Array(256);
+for (let i = 0; i < 256; i++) {
+  let c = i;
+  for (let j = 0; j < 8; j++) {
+    if (c & 1) {
+      c = 0xedb88320 ^ (c >>> 1);
+    } else {
+      c >>>= 1;
+    }
+  }
+  crcTable[i] = c;
+}
+
+export default function crc32(buffer: Uint8Array): number {
+  let crc = 0xffffffff;
+  const len = buffer.byteLength;
+
+  for (let i = 0; i < len; i++) {
+    crc = crcTable[(crc ^ buffer[i]) & 0xff] ^ (crc >>> 8);
+  }
+
+  return crc ^ 0xffffffff;
+}
