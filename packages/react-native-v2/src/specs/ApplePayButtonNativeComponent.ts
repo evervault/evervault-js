8ac23f03fa80e3a2b069b008a67aea62ef7b5250
Josh Pensky <joshuapensky@gmail.com>
2025-08-29 16:24:16 +0100

    use esm build for react-native field

--- packages/react-native-v2/src/specs/ApplePayButtonNativeComponent.ts
+++ packages/react-native-v2/src/specs/ApplePayButtonNativeComponent.ts
@@ -1,80 +1,85 @@
-import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
 import { HostComponent, ViewProps } from "react-native";
-import { DirectEventHandler, WithDefault } from "react-native/Libraries/Types/CodegenTypes";
+import { DirectEventHandler } from "react-native/Libraries/Types/CodegenTypes";
+import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
 
-export type CardNetwork = 'visa' | 'mastercard' | 'amex' | 'discover' | 'jcb';
+export type CardNetwork = "visa" | "mastercard" | "amex" | "discover" | "jcb";
 
 export class Amount {
-    public value: string;
-  
-    constructor(value: string) {
-      this.value = value;
-    }
-  };
-  
-  export type SummaryItem = {
-    label: string;
-    amount: string; //Amount;
-  };
-  
-  export type OneOffTransaction = {
-    currency: string;
-    country: string;
-    paymentSummaryItems: SummaryItem[];
-    // TODO: Add other fields
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
   }
-  
-  export type RecurringTransaction = {
-    // TODO: Add other fields
-  };
-  
-  export type DisbursementTransaction = {
-    // TODO: Add other fields
-  };
-  
-  export type Transaction = OneOffTransaction; //| RecurringTransaction | DisbursementTransaction;
-  
-  export type Config = {
-    appId: string;
-    merchantId: string;
-    supportedNetworks: string[]; //WithDefault<CardNetwork, 'visa'>[];
-    buttonType: string;
-    buttonTheme: string;
-  };
+}
 
-  export type ApplePayResponse = {
-    networkToken: {
-        number: string;
-        expiry: {
-            month: string;
-            year: string;
-        };
-        rawExpiry: string;
-        tokenServiceProvider: string;
-    };
-    card: {
-        brand?: string;
-        funding?: string;
-        segment?: string;
-        country?: string;
-        currency?: string;
-        issuer?: string;
-    };
-    cryptogram: string;
-    eci?: string;
-    paymentDataType: string;
-    deviceManufacturerIdentifier: string;
-  };
+export type SummaryItem = {
+  label: string;
+  amount: string; //Amount;
+};
 
-export type ApplePayNetworkToken = ApplePayResponse['networkToken'];
-export type ApplePayCard = ApplePayResponse['card'];
+export type OneOffTransaction = {
+  currency: string;
+  country: string;
+  paymentSummaryItems: SummaryItem[];
+  // TODO: Add other fields
+};
+
+export type RecurringTransaction = {
+  // TODO: Add other fields
+};
+
+export type DisbursementTransaction = {
+  // TODO: Add other fields
+};
+
+export type Transaction = OneOffTransaction; //| RecurringTransaction | DisbursementTransaction;
+
+export type Config = {
+  appId: string;
+  merchantId: string;
+  supportedNetworks: string[]; //WithDefault<CardNetwork, 'visa'>[];
+  buttonType: string;
+  buttonTheme: string;
+};
+
+export type ApplePayResponse = {
+  networkToken: {
+    number: string;
+    expiry: {
+      month: string;
+      year: string;
+    };
+    rawExpiry: string;
+    tokenServiceProvider: string;
+  };
+  card: {
+    brand?: string;
+    funding?: string;
+    segment?: string;
+    country?: string;
+    currency?: string;
+    issuer?: string;
+  };
+  cryptogram: string;
+  eci?: string;
+  paymentDataType: string;
+  deviceManufacturerIdentifier: string;
+};
+
+export type ApplePayNetworkToken = ApplePayResponse["networkToken"];
+export type ApplePayCard = ApplePayResponse["card"];
 
 export interface NativeProps extends ViewProps {
-    config: Config;
-    transaction: Transaction;
-    onDidAuthorizePayment?: DirectEventHandler<ApplePayResponse>;
-    onDidFinishWithResult?: DirectEventHandler<{ success: boolean; error?: string }>;
-    onPrepareTransaction?: DirectEventHandler<null>;
-  }
+  config: Config;
+  transaction: Transaction;
+  onDidAuthorizePayment?: DirectEventHandler<ApplePayResponse>;
+  onDidFinishWithResult?: DirectEventHandler<{
+    success: boolean;
+    error?: string;
+  }>;
+  onPrepareTransaction?: DirectEventHandler<null>;
+}
 
-export default codegenNativeComponent<NativeProps>('ApplePayButton') as HostComponent<NativeProps>;
+export default codegenNativeComponent<NativeProps>("ApplePayButton", {
+  excludedPlatforms: ["android"],
+}) as HostComponent<NativeProps>;
--- packages/react-native-v2/src/specs/GooglePayButtonNativeComponent.ts
+++ packages/react-native-v2/src/specs/GooglePayButtonNativeComponent.ts
@@ -1,39 +1,49 @@
 import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
 import { HostComponent, type ViewProps } from "react-native";
 
-export type ButtonType = 'book' | 'buy' | 'checkout' | 'donate' | 'order' | 'pay' | 'plain' | 'subscribe';
-export type ButtonTheme = 'light' | 'dark';
-export type AuthMethod = 'PAN_ONLY' | 'CRYPTOGRAM_3DS';
-export type CardNetwork = 'VISA' | 'MASTERCARD' | 'AMEX' | 'DISCOVER' | 'JCB';
+export type ButtonType =
+  | "book"
+  | "buy"
+  | "checkout"
+  | "donate"
+  | "order"
+  | "pay"
+  | "plain"
+  | "subscribe";
+export type ButtonTheme = "light" | "dark";
+export type AuthMethod = "PAN_ONLY" | "CRYPTOGRAM_3DS";
+export type CardNetwork = "VISA" | "MASTERCARD" | "AMEX" | "DISCOVER" | "JCB";
 
 export type Amount = {
-    value: string;
-  };
-  
-  export type LineItem = {
-    label: string;
-    amount: string; //Amount;
-  };
-  
-  export type Transaction = {
-    currency: string;
-    country: string;
-    total: string;
-    lineItems: LineItem[];
-  };
-  
-  export type Config = {
-    appId: string;
-    merchantId: string;
-    supportedNetworks: string[]; //CardNetwork[];
-    supportedMethods: string[]; //AuthMethod[];
-    buttonType: string; //ButtonType;
-    buttonTheme: string; //ButtonTheme;
-  };
-  
-export interface NativeProps extends ViewProps {
-    config: Config;
-    transaction: Transaction;
-  }
+  value: string;
+};
 
-export default codegenNativeComponent<NativeProps>('GooglePayButton') as HostComponent<NativeProps>;
+export type LineItem = {
+  label: string;
+  amount: string; //Amount;
+};
+
+export type Transaction = {
+  currency: string;
+  country: string;
+  total: string;
+  lineItems: LineItem[];
+};
+
+export type Config = {
+  appId: string;
+  merchantId: string;
+  supportedNetworks: string[]; //CardNetwork[];
+  supportedMethods: string[]; //AuthMethod[];
+  buttonType: string; //ButtonType;
+  buttonTheme: string; //ButtonTheme;
+};
+
+export interface NativeProps extends ViewProps {
+  config: Config;
+  transaction: Transaction;
+}
+
+export default codegenNativeComponent<NativeProps>("GooglePayButton", {
+  excludedPlatforms: ["iOS"],
+}) as HostComponent<NativeProps>;
