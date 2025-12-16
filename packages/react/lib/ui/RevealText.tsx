57e945f49d9ece735d2c14e47d4506be865571c8
Josh Pensky <joshuapensky@gmail.com>
2025-12-16 09:29:23 +0000

    Allow for customizing iframe `color-scheme` (#798)
    * add theme to GooglePayOptions
    
    * update example with dark color scheme
    
    * remove theme, move to ColorScheme option
    
    * validate colorscheme
    
    * add changeset
    
    * add React props
    
    * merge changsets

--- packages/react/lib/ui/RevealText.tsx
+++ packages/react/lib/ui/RevealText.tsx
@@ -2,18 +2,24 @@ import type Evervault from "@evervault/browser";
 import * as React from "react";
 import { useLayoutEffect, useRef, useState } from "react";
 import { useRevealContext } from "./RevealContext";
-import type { RevealFormat, ThemeDefinition } from "types";
+import type { ColorScheme, RevealFormat, ThemeDefinition } from "types";
 
 interface RevealTextProps {
   path: string;
   theme?: ThemeDefinition;
+  colorScheme?: ColorScheme;
   format?: RevealFormat;
 }
 
 type RevealInstance = ReturnType<Evervault["ui"]["reveal"]>;
 type RevealTextClass = ReturnType<RevealInstance["text"]>;
 
-export function RevealText({ path, theme, format }: RevealTextProps) {
+export function RevealText({
+  path,
+  theme,
+  format,
+  colorScheme,
+}: RevealTextProps) {
   const [instance, setInstance] = useState<RevealTextClass | null>(null);
   const ref = useRef<HTMLDivElement>(null);
   const { reveal } = useRevealContext();
@@ -23,10 +29,10 @@ export function RevealText({ path, theme, format }: RevealTextProps) {
     if (instance) return;
     if (!reveal) return;
 
-    const inst = reveal.text(path, { theme, format });
+    const inst = reveal.text(path, { colorScheme, theme, format });
     inst.mount(ref.current);
     setInstance(inst);
-  }, [reveal, path, theme, format, instance]);
+  }, [reveal, path, colorScheme, theme, format, instance]);
 
   return <div ref={ref} />;
 }
