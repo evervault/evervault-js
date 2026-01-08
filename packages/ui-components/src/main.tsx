87da912a1fe984238368d205e6315325496954f2
Josh Pensky <joshuapensky@gmail.com>
2026-01-08 15:26:15 +0000

    Move color-scheme checks to main bundle

--- packages/ui-components/src/main.tsx
+++ packages/ui-components/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Set the color scheme based on the query parameter
+const search = new URLSearchParams(window.location.search);
+const colorScheme = search.get("colorScheme");
+if (colorScheme) {
+  document.documentElement.style.colorScheme = colorScheme;
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
